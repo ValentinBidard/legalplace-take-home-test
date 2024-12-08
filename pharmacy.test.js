735bbe7db0f326655a3adb3c5435723b517bb998
Valentin <valentin.bidard@gmail.com>
2024-12-08 16:52:12 +0100

    ✅ Add tests for normal drugs

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,41 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)],
-    );
+  describe("Normal Drugs", () => {
+    it("should decrease the benefit and expiresIn", () => {
+      const drugs = [new Drug("test", 2, 3)];
+      const pharmacy = new Pharmacy(drugs);
+
+      pharmacy.updateBenefitValue();
+
+      expect(drugs[0]).toEqual(new Drug("test", 1, 2));
+    });
+
+    it("should decrease benefit and expiresIn by 1 per day", () => {
+      const drugs = [new Drug("test", 10, 20)];
+      const pharmacy = new Pharmacy(drugs);
+
+      pharmacy.updateBenefitValue();
+
+      expect(drugs[0]).toEqual(new Drug("test", 9, 19));
+    });
+
+    it("should decrease benefit by 2 after expiration", () => {
+      const drugs = [new Drug("test", 0, 20)];
+      const pharmacy = new Pharmacy(drugs);
+
+      pharmacy.updateBenefitValue();
+
+      expect(drugs[0]).toEqual(new Drug("test", -1, 18));
+    });
+
+    it("should not decrease benefit below 0", () => {
+      const drugs = [new Drug("test", 10, 0)];
+      const pharmacy = new Pharmacy(drugs);
+
+      pharmacy.updateBenefitValue();
+
+      expect(drugs[0]).toEqual(new Drug("test", 9, 0));
+    });
   });
 });
