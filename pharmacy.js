29f96630c3adc62652102894ea69cdb9be89e0a9
Valentin <valentin.bidard@gmail.com>
2024-12-08 16:41:52 +0100

    ♻️ Refactor UpdateBenefitValue

--- pharmacy.js
+++ pharmacy.js
@@ -82,57 +82,35 @@ export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
-  updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
-        }
-      }
-    }
 
+  /**
+   * @description
+   * Get the strategy to update the benefit of a drug
+   * @param {Drug} drug
+   * @returns {DrugStrategy}
+   */
+  getStrategy(drug) {
+    switch (drug.name) {
+      case "Herbal Tea":
+        return new HerbalTeaStrategy(drug);
+      case "Magic Pill":
+        return new MagicPillStrategy(drug);
+      case "Fervex":
+        return new FervexStrategy(drug);
+      case "Dafalgan":
+        return new DafalganStrategy(drug);
+      default:
+        return new DrugStrategy(drug);
+    }
+  }
+
+  /**
+   * @description
+   * Update the benefit of each drug
+   * @returns {Drug[]}
+   */
+  updateBenefitValue() {
+    this.drugs.forEach((drug) => this.getStrategy(drug).update());
     return this.drugs;
   }
 }
